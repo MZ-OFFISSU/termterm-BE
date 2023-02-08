70c3ebb4cbdeaa6e6dd72c501d4f933b5433cb86
thisIsJooS <thisis.joos@gmail.com>
2023-02-08 20:25:08 +0900

    테스트코드 host 주소 yml 매핑

--- src/test/java/com/mzoffissu/termterm/controller/auth/AuthControllerTest.java
+++ src/test/java/com/mzoffissu/termterm/controller/auth/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -18,6 +19,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -29,8 +33,11 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
 class AuthControllerTest {
+    @Value("${server.host}")
+    private String HOST;
+
     @LocalServerPort
-    private int port;
+    private int PORT;
 
     @Autowired
     private WebApplicationContext context;
@@ -52,9 +59,8 @@ class AuthControllerTest {
     public void wrongSocialType() throws Exception{
         //given
         JSONParser parser = new JSONParser();
-        String host = "http://localhost:";
         String path = "/auth/something";
-        String url = host + port + path;
+        String url = HOST + PORT + path;
         WrongSocialTypeRequestDto requestDto = WrongSocialTypeRequestDto.builder()
                 .code("adsd")
                 .build();
@@ -75,9 +81,8 @@ class AuthControllerTest {
     public void noCodeInBody() throws Exception{
         //given
         JSONParser parser = new JSONParser();
-        String host = "http://localhost:";
         String path = "/auth/kakao";
-        String url = host + port + path;
+        String url = HOST + PORT + path;
         WrongDataDto requestDto = WrongDataDto.builder()
                 .cade("daa")
                 .build();
