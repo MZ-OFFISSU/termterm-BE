e44c08e01e1d893c6fd95d131fdc9ab946512a09
thisIsJooS <thisis.joos@gmail.com>
2023-02-17 14:09:46 +0900

    feat: 문의사항 접수 응답메시지

--- src/main/java/com/mzoffissu/termterm/vo/ResponseMessage.java
+++ src/main/java/com/mzoffissu/termterm/vo/ResponseMessage.java
@@ -2,4 +2,5 @@ package com.mzoffissu.termterm.vo;
 
 public class ResponseMessage {
     public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String INQUIRY_ACCEPTED = "문의 접수 완료";
 }
