13aa52bbf1daba664bc28c5fc7f0f9c89fbcde57
Seungwoo, Joo <thisis.joos@gmail.com>
2023-03-06 16:50:39 +0900

    refactor: 에러 response 메시지 수정

--- src/main/java/com/mzoffissu/termterm/exception/ExceptionHandler.java
+++ src/main/java/com/mzoffissu/termterm/exception/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mzoffissu.termterm.exception;
 
+import com.mzoffissu.termterm.dto.DefaultResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(BizException.class)
-    public ResponseEntity<ErrorResult> bizException(BizException e){
-        return new ResponseEntity<>(ErrorResult.create(e.getBaseExceptionType()), e.getBaseExceptionType().getHttpStatus());
+    public ResponseEntity bizException(BizException e){
+        return new ResponseEntity<>(DefaultResponse.create(e.getBaseExceptionType().getHttpStatus().value(), e.getMessage()), e.getBaseExceptionType().getHttpStatus());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResult> notResolvedException(Exception e){
-        return new ResponseEntity<>(ErrorResult.create(InternalServerExceptionType.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity notResolvedException(Exception e){
+        return new ResponseEntity<>(DefaultResponse.create(InternalServerExceptionType.INTERNAL_SERVER_ERROR.getHttpStatus().value(), InternalServerExceptionType.INTERNAL_SERVER_ERROR.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
